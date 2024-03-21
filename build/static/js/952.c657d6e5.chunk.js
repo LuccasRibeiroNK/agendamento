74e7a1bdb9b1e4e53ce7f821234ef20b745e0c56
LuccasRibeiroNK <luckgl@hotmail.com>
2024-03-21 16:27:10 -0300

    build commit

---
+++ build/static/js/952.c657d6e5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkagendamento=self.webpackChunkagendamento||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.c657d6e5.chunk.js.map
